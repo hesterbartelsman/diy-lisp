0347780f41c119d4b1c010669dccc8387d5ea129
Hester Bartelsman <hbartelsman@xs4all.nl>
2014-05-16 13:26:08 +0200

    evaluator-not finished

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -5,6 +5,7 @@ from ast import is_boolean, is_atom, is_symbol, is_list, is_closure, is_integer
 from asserts import assert_exp_length, assert_valid_definition, assert_boolean
 from parser import unparse
 
+#definitely not final version. Haven't done anything with the environment yet...
 """
 This is the Evaluator module. The `evaluate` function below is the heart
 of your language, and the focus for most of parts 2 through 6.
@@ -16,4 +17,57 @@ in a day, after all.)
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
-    raise NotImplementedError("DIY")
+    left_value = ast.left.evaluate(env)
+    right_value = self.right.evaluate(env)
+    if is_boolean()
+    if ast[0] == 'quote':
+        return ast[1]
+    if ast[0] == 'atom':
+        return is_atom(ast[1])
+    if ast[0] == 'eq':
+        value = left_value == right_value
+    if is_integer(left_value) and is_integer(right_value):
+        if ast.op == '+':
+            value = left_value + right_value
+        elif ast.op == '-':
+            value = left_value - right_value
+        elif ast.op == '*':
+            value = left_value * right_value
+        elif ast.op == '/':
+            value = left_value / right_value
+        elif ast.op == '%'
+            value = left_value % right_value
+        elif ast.op == '>'
+            value = left_value > right_value
+    else:
+        raise NotImplementedError("DIY")
+    return value
+
+
+def evaluate(ast, env):
+    """Evaluate an Abstract Syntax Tree in the specified environment."""
+    left_value = ast.left.evaluate(env)
+    right_value = self.right.evaluate(env)
+    if is_boolean()
+    if ast[0] == 'quote':
+        return ast[1]
+    if ast[0] == 'atom':
+        return is_atom(ast[1])
+    if ast[0] == 'eq':
+        value = ast[1] == ast[2]
+    if is_integer(ast[1]) and is_integer(ast[2]):
+        if ast.op == '+':
+            value = ast[1] + ast[2]
+        elif ast.op == '-':
+            value = ast[1] - ast[2]
+        elif ast.op == '*':
+            value = ast[1] * ast[2]
+        elif ast.op == '/':
+            value = ast[1] / ast[2]
+        elif ast.op == '%'
+            value = ast[1] % ast[2]
+        elif ast.op == '>'
+            value = ast[1] > ast[2]
+    else:
+        raise NotImplementedError("DIY")
+    return value
