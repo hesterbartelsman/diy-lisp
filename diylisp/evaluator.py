de32519b737c798f445ced87f7830a6509cc3d40
Hester Bartelsman <hbartelsman@xs4all.nl>
2014-05-22 16:31:47 +0200

    head, tail, empty included. cons still missing

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -21,16 +21,18 @@ in a day, after all.)
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
-    left_value = ast.left.evaluate(env)
-    right_value = self.right.evaluate(env)
-    if is_boolean()
+
+    if is_boolean(ast) or is_integer(ast):
+        return = ast
+    else:
+        raise Error("Wrong input: input must be boolean or integer")
     if ast[0] == 'quote':
-        return ast[1]
-    if ast[0] == 'atom':
-        return is_atom(ast[1])
-    if ast[0] == 'eq':
-        value = ast[1] == ast[2]
-    if is_integer(ast[1]) and is_integer(ast[2]):
+        value = ast[1]
+    elif ast[0] == 'atom':
+        value = is_atom(ast[1])
+    elif ast[0] == 'eq':
+        value = is_atom(evaluate(ast[1])) == is_atom(evaluate(ast[2]))
+    elif is_integer(ast[1]) and is_integer(ast[2]):
         if ast.op == '+':
             value = evaluate(ast[1], env) + evaluate(ast[2], env)
         elif ast.op == '-':
@@ -39,11 +41,11 @@ def evaluate(ast, env):
             value = evaluate(ast[1], env) * evaluate(ast[2], env)
         elif ast.op == '/':
             value = evaluate(ast[1], env) / evaluate(ast[2], env)
-        elif ast.op == '%'
+        elif ast.op == '%':
             value = evaluate(ast[1], env) % evaluate(ast[2], env)
-        elif ast.op == '>'
+        elif ast.op == '>':
             value = evaluate(ast[1], env) > evaluate(ast[2], env)
-    if ast[0] == 'if':
+    elif ast[0] == 'if':
         if not is_boolean(ast[1]):
             return 'Error: expected the statement to evaluate to a boolean'
         if is_boolean(ast[1]):
@@ -51,10 +53,19 @@ def evaluate(ast, env):
                 value = evaluate(ast[2], env)
             else:
                 value = evaluate(ast[3], env)
-    if ast[0] == 'def' :
+    elif ast[0] == 'def' :
         value = evalDef(ast[1:], env)
-    if ast[0] == 'lambda' :
+    elif ast[0] == 'lambda' :
         value = evalLam(ast[1:], env)
+    elif ast[0] == 'cons':
+        value = evalCons(ast[1:], env)
+    elif ast[0] == 'head':
+        value = evalHead(ast[1:], env)
+    elif ast[0] == 'tail':
+        value = evalTail(ast[1:], env)
+    elif ast[0] == 'empty':
+        value = evalEmpty(ast[1:], env)
+
     else:
         raise NotImplementedError("DIY")
 
@@ -75,12 +86,25 @@ def evalLam(ast, env)
     body = ast[1]
     if is_list(params):
         #rest of function
-        #should return some value.. 
+        #should return some value..
     else:
         raise LispError("parameters not a list")
 
+def evalCons(ast, env):
 
 
+
+def evalHead(ast, env):
+    headTail = evaluate(ast[0])
+    return headTail[0]
+
+def evalTail(ast, env):
+    headTail = evaluate(ast[0])
+    return headTail[1:]
+
+def evalEmpty(ast, eng):
+    emptyList = evaluate(ast[0])
+    return emptyList == []
 
     
 
