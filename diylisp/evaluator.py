dbe5fdbb8d266e88c4b81e4df334b512718223f6
Hester Bartelsman <hbartelsman@xs4all.nl>
2014-05-23 23:10:27 +0200

    now also took wisse's evaluater and I still have problems with tests.

--- diylisp/evaluator.py
+++ diylisp/evaluator.py
@@ -1,122 +1,168 @@
-# -*- coding: utf-8 -*-
-
 from types import Environment, LispError, Closure
 from ast import is_boolean, is_atom, is_symbol, is_list, is_closure, is_integer
 from asserts import assert_exp_length, assert_valid_definition, assert_boolean
 from parser import unparse
+import re as re
+from operator import add, sub, mul, div, gt, lt, ge, le, mod
 
-#definitely not final version. Haven't done anything with the environment yet...
 """
 This is the Evaluator module. The `evaluate` function below is the heart
 of your language, and the focus for most of parts 2 through 6.
 
-A score of useful functions is provided for you, as per the above imports, 
-making your work a bit easier. (We're supposed to get through this thing 
+A score of useful functions is provided for you, as per the above imports,
+making your work a bit easier. (We're supposed to get through this thing
 in a day, after all.)
 """
 
-#definitely handier to split functions and have the evaluator call the separate funcitons
-#I'll leave what I did for last assignment as it was though.
-#I didn't do the tests all in order. I chose the ones I understand.
+integer = re.compile('[0-9]+')
+symbol = re.compile('[a-zA-Z*<>+=-]+')
+
+#This is also Wisse's code!
 
 def evaluate(ast, env):
     """Evaluate an Abstract Syntax Tree in the specified environment."""
 
-#    if is_boolean(ast) or is_integer(ast):
+    # evaluating atoms
+    if is_symbol(ast):
+        return env.lookup(ast)
+    if is_boolean(ast):
+        return ast
+    if is_integer(ast):
+        return ast
 
-    if ast[0] == 'quote':
-        value = ast[1]
-    elif ast[0] == 'atom':
-        value = is_atom(ast[1])
-    elif ast[0] == 'eq':
-        value = is_atom(evaluate(ast[1], env)) == is_atom(evaluate(ast[2], env))
-    elif is_integer(ast[1]) and is_integer(ast[2]):
-        if ast.op == '+':
-            value = evaluate(ast[1], env) + evaluate(ast[2], env)
-        elif ast.op == '-':
-            value = evaluate(ast[1], env) - evaluate(ast[2], env)
-        elif ast.op == '*':
-            value = evaluate(ast[1], env) * evaluate(ast[2], env)
-        elif ast.op == '/':
-            value = evaluate(ast[1], env) / evaluate(ast[2], env)
-        elif ast.op == '%':
-            value = evaluate(ast[1], env) % evaluate(ast[2], env)
-        elif ast.op == '>':
-            value = evaluate(ast[1], env) > evaluate(ast[2], env)
-    elif ast[0] == 'if':
-        if not is_boolean(ast[1]):
-            return 'Error: expected the statement to evaluate to a boolean'
-        if is_boolean(ast[1]):
-            if ast[2] == True:
-                value = evaluate(ast[2], env)
+    if is_list(ast):
+
+        # lists
+        if ast[0] == "cons":
+            if len(ast) != 3:
+                raise LispError("expected 2 arguments")
             else:
-                value = evaluate(ast[3], env)
-    elif ast[0] == 'define' :
-        value = evalDef(ast[1:], env)
-    elif ast[0] == 'lambda' :
-        value = evalLam(ast[1:], env)
-    elif ast[0] == 'cons':
-        value = evalCons(ast[1:], env)
-    elif ast[0] == 'head':
-        value = evalHead(ast[1:], env)
-    elif ast[0] == 'tail':
-        value = evalTail(ast[1:], env)
-    elif ast[0] == 'empty':
-        value = evalEmpty(ast[1:], env)
+                value = evaluate(ast[1], env)
+                list = evaluate(ast[2], env)
+                new_list = [value]
+                for i in list:
+                    new_list.append(i)
+                return new_list
 
-    else:
-        raise NotImplementedError("DIY")
-    return value
+        if ast[0] == "head":
+            list = evaluate(ast[1], env)
+            if list == []:
+                raise LispError
+            else: return list[0]
 
+        if ast[0] == "tail":
+            list = evaluate(ast[1], env)
+            if list == []:
 
+                raise LispError
+            else: return list[1:]
 
+        if ast[0] == "empty":
+            list = evaluate(ast[1], env)
+            if list == []:
+                return True
+            else: return False
 
-
-
-def evalDef(ast, env):
-    assert_valid_definition(ast)
-    symbol = ast[0]
-    value = evaluate(ast[1],env)
-    env.set(symbol,value)
-    return symbol
-
-#def evalLam(ast, env):
-#    params = ast[0]
-#    body = ast[1]
-#    if is_list(params):
-#        #rest of function
-#        #should return some value..
-#    else:
-#        raise LispError("parameters not a list")
-
-def evalCons(ast, env):
-    head = evaluate(ast[0], env)
-    tail = evaluate(ast[1], env)
-    return tail.insert(0,head)
-
-def evalHead(ast, env):
-    headTail = evaluate(ast[0], env)
-    return headTail[0]
-
-def evalTail(ast, env):
-    headTail = evaluate(ast[0], env)
-    return headTail[1:]
-
-def evalEmpty(ast, env):
-    emptyList = evaluate(ast[0], env)
-    return emptyList == []
-
-    
+        if ast[0] == "quote":
+            return ast[1]
+        # functions
 
 
+        if is_closure(ast[0]):
+            closure = ast[0]
+            arguments = ast[1:]
+            params = closure.params
+            number_of_arguments = len(arguments)
+            number_of_params = len(params)
 
+            if number_of_arguments != number_of_params:
+                raise LispError("wrong number of arguments, expected %(param)d got %(arg)d" %
+                                {"arg": number_of_arguments, "param": number_of_params})
+            variables = {}
+            for i in range(number_of_arguments):
+                arg = evaluate(arguments[i], env)
+                param = params[i]
+                variables.update({param: arg})
+            environment = closure.env.extend(variables)
 
+            return evaluate(closure.body, environment)
 
+        if ast[0] == "lambda":
+            if not is_list(ast[1]):
+                raise LispError
+            if len(ast) == 3:
+                return Closure(env, ast[1], ast[2])
+            else:
+                raise LispError("number of arguments")
 
+        # defining variables
+        if ast[0] == "define":
+            if is_symbol(ast[1]):
+                if len(ast) == 3:
+                    return env.set(ast[1], evaluate(ast[2], env))
+                else:
+                    raise LispError("Wrong number of arguments")
+            else:
+                raise LispError("non-symbol")
 
-    #for the conditional evaluation:
-    #somehow that when the if statement does not evaluate to a boolean:
-    #give error: expected a boolean for conditional statment
-    #when if statement does evaluate to a boolean:
-    #give: if conditional evaluates to true: evaluate second part of if statement
-    #give: if conditional evaluate to false: evaluate third part of if statement
+        #typechecks
+        if ast[0] == "atom":
+            return is_atom(evaluate(ast[1], env))
+        if ast[0] == "eq":
+            return evaluate(ast[1], env) == evaluate(ast[2], env) and \
+                   is_atom(evaluate(ast[1], env)) and is_atom(evaluate(ast[2], env))
+
+        #arithmetic:
+        # elif is_arith_op(ast[0]):
+        #     try:
+        #         return arith_ops[ast[0]](evaluate(ast[1], env), evaluate(ast[2], env))
+        # make dicitonary of these operators
+
+        if ast[0] == "+":
+            if is_integer(evaluate(ast[1], env)) and is_integer(evaluate(ast[2], env)):
+                return evaluate(ast[1], env) + evaluate(ast[2], env)
+            else:
+                raise LispError
+        if ast[0] == "-":
+            if is_integer(evaluate(ast[1], env)) and is_integer(evaluate(ast[2], env)):
+                return evaluate(ast[1], env) - evaluate(ast[2], env)
+            else:
+                raise LispError
+        if ast[0] == "*":
+            if is_integer(evaluate(ast[1], env)) and is_integer(evaluate(ast[2], env)):
+                return evaluate(ast[1], env) * evaluate(ast[2], env)
+            else:
+                raise LispError
+        if ast[0] == "/":
+            if is_integer(evaluate(ast[1], env)) and is_integer(evaluate(ast[2], env)):
+                return evaluate(ast[1], env) / evaluate(ast[2], env)
+            else:
+                raise LispError
+        if ast[0] == "mod":
+            if is_integer(evaluate(ast[1], env)) and is_integer(evaluate(ast[2], env)):
+                return evaluate(ast[1], env) % evaluate(ast[2], env)
+            else:
+                raise LispError
+
+        # boolean operators
+        if ast[0] == ">":
+            return evaluate(ast[1], env) > evaluate(ast[2], env)
+        if ast[0] == "<":
+            return evaluate(ast[1], env) < evaluate(ast[2], env)
+
+        # control-flow
+        if ast[0] == 'if':
+            pred = ast[1]
+            then = ast[2]
+            elsee = ast[3]
+            if evaluate(pred, env):
+                return evaluate(then, env)
+            else:
+                return evaluate(elsee, env)
+
+        if is_symbol(ast[0]) or is_list(ast[0]):
+            closure = evaluate(ast[0], env)
+            return evaluate([closure] + ast[1:], env)
+
+        else:
+            raise LispError("not a function")
